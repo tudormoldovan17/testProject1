be622ff6b6166d3bb985de92f79d7e7eb1b2e43a
tudormoldovan17 <infiniteblazexd@yahoo.com>
2021-09-28 10:12:15 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+a = input("Introduce the first number : ")
+b = input("Introduce the second number : ")
+print(a)
+print(b)
+print(a+b);
+
+a = int(a)
+b = int(b)
+
+print(a+b)
