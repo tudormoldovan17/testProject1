5f44860e7322a55cdecb83a6a45de79c7311ff20
tudormoldovan17 <infiniteblazexd@yahoo.com>
2021-09-28 10:28:46 +0300

    yes

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 print("Hello World!")
-a = input("Introduce the first number : ")
-b = input("Introduce the second number : ")
-print(a)
-print(b)
-print(a+b);
+#a = input("Introduce the first number : ")
+#b = input("Introduce the second number : ")
+#print(a)
+#print(b)
+#print(a+b);
 
-a = int(a)
-b = int(b)
+#a = int(a)
+#b = int(b)
 
-print(a+b)
+#rint(a+b)
+
+myList = [10,20,30,40]
+print(myList)
+print(myList[1:3])
+print(myList[2:4])
+print(myList[:3]);
